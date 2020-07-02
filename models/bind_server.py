7068b839cb79446009dd4c4d90a37de032c346ff
Zhiyang-Zhang-sjsu <zhiyang.zhang@sjsu.edu>
2020-07-02 10:12:33 +0000

    add date and time variables

--- models/bind_server.py
+++ models/bind_server.py
@@ -1,14 +1,15 @@
 from db import db
-from sqlalchemy.schema import Sequence
 
 class BindServer(db.Model):
     __tablename__ = 'server_monitored'
 
-    id = db.Column(db.Integer, Sequence('bind_server_id'))
+    id = db.Column(db.Integer)
+    register_date = db.Column(db.Date, nullable=False)
+    register_time = db.Column(db.Time, nullable=False)
     first_name = db.Column(db.String(30), primary_key=True)
     last_name = db.Column(db.String(20), primary_key=True)
-    email = db.Column(db.String(60))
-    phone = db.Column(db.String(11))
+    email = db.Column(db.String(60), nullable=False)
+    phone = db.Column(db.String(11), nullable=False)
     address = db.Column(db.String(100))
     city = db.Column(db.String(80))
     state = db.Column(db.String(30))
@@ -17,10 +18,15 @@ class BindServer(db.Model):
     host_user = db.Column(db.String(20), primary_key=True)
     description = db.Column(db.String(300))
 
-    def __init__(self, first_name, last_name, email,\
+    def __init__(self, register_datetime,\
+            first_name, last_name, email,\
             phone, address, city, state, zip_code,\
             host, host_user, description):
 
+        date = register_datetime.split(' ')[0].split('/')
+        time = register_datetime.split(' ')[1].split(':')
+        self.register_date = str(date[2]) + str(date[0]).zfill(2) + str(date[1]).zfill(2)
+        self.register_time = str(time[0]).zfill(2) + str(time[1]).zfill(2) + str(time[2]).zfill(2)
         self.first_name = first_name 
         self.last_name = last_name 
         self.email = email 
